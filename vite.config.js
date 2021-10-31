1accd1a42e5cd6e685c4b44182005df65c94bf36
mutablestate <benwebdev@gmail.com>
2021-10-31 10:45:07 +0700

    fix(build): cloudflare hmr
    add compiled folder

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,15 @@
-import { defineConfig } from 'vite'
-import { svelte } from '@sveltejs/vite-plugin-svelte'
+import { defineConfig } from "vite";
+import { svelte } from "@sveltejs/vite-plugin-svelte";
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [svelte()]
-})
+export default defineConfig(({ command, mode }) => {
+  const isProd = mode === "production";
+
+  return {
+    plugins: [
+      svelte({
+        hot: !isProd,
+      }),
+    ],
+  };
+});
